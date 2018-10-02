a68c8ec4d3c11ca3eb3f71f7fe3367f9104e6656
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-10-02 08:49:45 -0700

    Agregar archivos de proyecto.

---
+++ PracticaNuevoUsuario/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PracticaNuevoUsuario
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            paneldatos.Children.Clear();
+
+            switch (CBTipoUsuario.SelectedIndex)
+            {
+                case 0:
+                    paneldatos.Children.Add(new controldatosalumno());
+                    break;
+
+                case 1:
+                    paneldatos.Children.Add(new controldatosmaestro());
+                    break;
+            }
+        }
+    }
+}
---
+++ PracticaNuevoUsuario/controldatosmaestro.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PracticaNuevoUsuario
+{
+    /// <summary>
+    /// Lógica de interacción para controldatosmaestro.xaml
+    /// </summary>
+    public partial class controldatosmaestro : UserControl
+    {
+        public controldatosmaestro()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGuardar_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
